a352122d6bd4fa61242eedfe2c7f3566f2906303
MEugeV <eugeniavogt@yahoo.com.ar>
2022-09-17 01:15:14 -0300

    sync

--- api/index.js
+++ api/index.js
@@ -25,8 +25,8 @@ const {
 require("dotenv").config();
 
 // Syncing all the models at once .
-conn.sync({ force: true }).then(() => {
-  createTemperaments();
+conn.sync().then(() => {
+  // createTemperaments();
   server.listen(process.env.PORT, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
