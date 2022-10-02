3841d6e7c3d3a7b0fe9e5ad66b7062caeba0640b
MEugeV <eugeniavogt@yahoo.com.ar>
2022-10-02 18:47:18 -0300

    responsive

--- client/src/containers/home.js
+++ client/src/containers/home.js
@@ -1,100 +1,105 @@
-import React, {useEffect} from "react";
-import {connect} from "react-redux"
-import Dogs from "../components/dogs"
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import Dogs from "../components/dogs";
 import CardsBar from "./cardsBar";
-import {getDogs, getTemperaments, setPage, resetDogs} from "../redux/actions" 
+import { getDogs, getTemperaments, setPage, resetDogs } from "../redux/actions";
 import { Paginado } from "../components/paginado";
-import styles from "../styles/home.module.css"
-import img from "../styles/pictures/breednf.jpg" 
+import styles from "../styles/home.module.css";
+import img from "../styles/pictures/breednf.jpg";
 import NavBar from "../components/navBar";
 
-function Home ({shownDogs, temperaments, dogs,getDogs,getTemperaments,page, setPage,resetDogs}) { 
+function Home({
+  shownDogs,
+  temperaments,
+  dogs,
+  getDogs,
+  getTemperaments,
+  page,
+  setPage,
+  resetDogs,
+}) {
+  const [isLoading, setIsloading] = useState(true);
+  const [getError, setGetError] = useState(false);
 
-
-useEffect (()=>{
+  useEffect(() => {
     async function upDatePost() {
-        await getDogs()
-        getTemperaments()
+      try {
+        await getDogs();
+      } catch (error) {
+        setGetError(true);
+      }
+      setIsloading(false);
+      getTemperaments();
     }
-    upDatePost()
-    return ()=>{
-        resetDogs()
-        setPage(1)
-    }
-},[])
- 
+    upDatePost();
+    return () => {
+      resetDogs();
+      setPage(1);
+    };
+  }, []);
 
+  let pagesPerPage = 8;
+  let pages = Math.ceil(shownDogs.length / pagesPerPage);
 
-let pagesPerPage=8
-let pages=Math.ceil(shownDogs.length/pagesPerPage)
+  function handlePage(e) {
+    setPage(e.target.value);
+  }
 
-function handlePage (e) {
-    setPage(e.target.value)
-}
+  function handlePrev(e) {
+    setPage(parseInt(page) - 1);
+  }
 
-function handlePrev (e) {
-    setPage(parseInt(page)-1)
-}
+  function handleNext(e) {
+    setPage(parseInt(page) + 1);
+  }
 
-function handleNext (e) {
-    setPage(parseInt(page)+1)
-}
-
-
-    return (
-        <div className={styles.home}>
-            <NavBar>    
-            </NavBar>
-            <div className={styles.homeBar}>
-                <CardsBar temperaments={temperaments}>
-                </CardsBar> 
-                <Paginado 
-                    page={page} 
-                    pages={pages} 
-                    handlePage={handlePage} 
-                    handleNext={handleNext} 
-                    handlePrev={handlePrev}>
-                </Paginado>
-            </div>
-            {shownDogs.length===0
-            ? dogs.length===0
-            ? <div 
-                className={styles.loading}>
-                    Loading....
-                    <span 
-                        className={styles.loadingIn} >
-                            &#160;
-                    </span>
-                </div> 
-            : <div 
-                className={styles.notFound}>
-                    <div className={styles.positionNotFound}>" "</div>
-                    <p >Empty selection...</p> 
-                    <img 
-                        src={img} 
-                        className={styles.imgNotFound} 
-                        alt="bnf"/>
-            </div> 
-            : <div className={styles.positionDogsCont}>
-                <div className={styles.positionDogs}>" "</div>
-                <Dogs 
-                    dogs={shownDogs} 
-                    page={page} 
-                    pagesPerPage={pagesPerPage}>
-                </Dogs>      
-            </div>}
+  return (
+    <div className={styles.home}>
+      <NavBar></NavBar>
+      <div className={styles.homeBar}>
+        <CardsBar temperaments={temperaments}></CardsBar>
+        <Paginado
+          page={page}
+          pages={pages}
+          handlePage={handlePage}
+          handleNext={handleNext}
+          handlePrev={handlePrev}
+        ></Paginado>
+      </div>
+      {isLoading ? (
+        <div className={styles.loading}>
+          Loading....
+          <span className={styles.loadingIn}>&#160;</span>
         </div>
-    )
-
+      ) : getError ? (
+        <div className={styles.notFound}>
+          <div className={styles.positionNotFound}>" "</div>
+          <p>Status 404</p>
+          <p>Not Found</p>
+          <img src={img} className={styles.imgNotFound} alt="bnf" />
+        </div>
+      ) : (
+        <div className={styles.positionDogsCont}>
+          <div className={styles.positionDogs}>" "</div>
+          <Dogs dogs={shownDogs} page={page} pagesPerPage={pagesPerPage}></Dogs>
+        </div>
+      )}
+    </div>
+  );
 }
 
 function mapStateToProps(state) {
-    return {
-        shownDogs: state.shownDogs,
-        dogs: state.dogs,
-        page: state.page,
-        temperaments: state.temperaments,
-    }
+  return {
+    shownDogs: state.shownDogs,
+    dogs: state.dogs,
+    page: state.page,
+    temperaments: state.temperaments,
+  };
 }
 
-export default connect (mapStateToProps, {getDogs,getTemperaments, setPage, resetDogs}) (Home)
+export default connect(mapStateToProps, {
+  getDogs,
+  getTemperaments,
+  setPage,
+  resetDogs,
+})(Home);
--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,82 +1,80 @@
-import axios from "axios"
+import axios from "axios";
 
-export const GET_DOGS= "GET_DOGS" 
-export const GET_TEMPERAMENTS= "GET_TEMPERAMENTS" 
-export const GET_DETAIL= "GET_DETAIL" 
-export const RESET_DETAIL= "RESET_DETAIL" 
-export const SET_PAGE= "SET_PAGE"
-export const RESET_DOGS= "RESET_DOGS"
-export const SEARCH_NAME= "SEARCH_NAME"
-export const FILTER_DOGS= "FILTER_DOGS"
-export const ORDER_DOGS= "ORDER_DOGS"
-
-
+export const GET_DOGS = "GET_DOGS";
+export const GET_TEMPERAMENTS = "GET_TEMPERAMENTS";
+export const GET_DETAIL = "GET_DETAIL";
+export const RESET_DETAIL = "RESET_DETAIL";
+export const SET_PAGE = "SET_PAGE";
+export const RESET_DOGS = "RESET_DOGS";
+export const SEARCH_NAME = "SEARCH_NAME";
+export const FILTER_DOGS = "FILTER_DOGS";
+export const ORDER_DOGS = "ORDER_DOGS";
 
 export function getDogs() {
-    return (dispatch) =>{
-            return axios("/dogs")
-            .then(data=> dispatch({type: GET_DOGS, payload: data.data }))
-            .catch(error=>alert(error.message))
-    }
+  return (dispatch) => {
+    return axios("/dogs").then((data) =>
+      dispatch({ type: GET_DOGS, payload: data.data })
+    );
+    //   .catch((error) => alert(error.message));
+  };
 }
 
 export function getTemperaments() {
-    return (dispatch) => {
-            return axios("/temperaments")
-            .then(data=>dispatch({type: GET_TEMPERAMENTS, payload: data.data}))
-            .catch(error=>alert(error.message))
+  return (dispatch) => {
+    return axios("/temperaments")
+      .then((data) => dispatch({ type: GET_TEMPERAMENTS, payload: data.data }))
+      .catch((error) => alert(error.message));
+  };
+}
+
+export function getDetail(id) {
+  return async function (dispatch) {
+    try {
+      const data = await axios(`/dogs/${id}`);
+      dispatch({ type: GET_DETAIL, payload: data.data });
+    } catch (error) {
+      alert("We could´t find this detail" + error.message);
     }
+  };
 }
 
-export function getDetail (id) {
-    return async function (dispatch) {
-        try {
-            const data= await axios(`/dogs/${id}`)
-            dispatch({type: GET_DETAIL, payload: data.data })
-        } catch(error) {
-            alert("We could´t find this detail" + error.message)
-        }
-    }
+export function resetDetail() {
+  return { type: RESET_DETAIL };
 }
 
-export function resetDetail () {
-    return {type: RESET_DETAIL}
+export function setPage(page) {
+  return { type: SET_PAGE, payload: page };
 }
 
-export function setPage (page) {
-    return {type: SET_PAGE, payload: page}
+export function resetDogs() {
+  return { type: RESET_DOGS };
 }
 
-export function resetDogs () {
-    return {type: RESET_DOGS}
-}
- 
 export function searchName(name) {
-    return async function (dispatch){
-        try{
-            const data= await axios.get(`/dogs`,{params:{name}})
-            dispatch({type:SEARCH_NAME, payload: data.data})
-        } catch (error) {
-            alert(error.request.response)
-            dispatch({type: RESET_DOGS})
-        }
+  return async function (dispatch) {
+    try {
+      const data = await axios.get(`/dogs`, { params: { name } });
+      dispatch({ type: SEARCH_NAME, payload: data.data });
+    } catch (error) {
+      alert(error.request.response);
+      dispatch({ type: RESET_DOGS });
     }
+  };
 }
 
-export function filterDogs (filter) {
-    return {type: FILTER_DOGS, payload: filter}
+export function filterDogs(filter) {
+  return { type: FILTER_DOGS, payload: filter };
 }
 
-export function orderDogs (order) {
-    return {type: ORDER_DOGS, payload: order}
+export function orderDogs(order) {
+  return { type: ORDER_DOGS, payload: order };
 }
 
 export async function postDog(created) {
-    try {
-        const response= await axios.post(`/dogs`,created)
-        alert(response.request.response)
-    } catch (error) {
-        alert("Dog not created... "+ error.request.response)
-    }
+  try {
+    const response = await axios.post(`/dogs`, created);
+    alert(response.request.response);
+  } catch (error) {
+    alert("Dog not created... " + error.request.response);
+  }
 }
-
